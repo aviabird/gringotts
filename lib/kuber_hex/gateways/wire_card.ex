e9a82d8456b4a8bbeb34b4cf80f0cf2fe16ff7fa
voidzero <kartikjagdale11@gmail.com>
2017-12-05 11:42:55 +0530

    add barebone for wirecard library

---
+++ lib/kuber_hex/gateways/wire_card.ex
@@ -0,0 +1,6 @@
+defmodule Kuber.Hex.Gateways.WireCard do
+  @test_url 'https://c3-test.wirecard.com/secure/ssl-gateway'
+  @live_url 'https://c3.wirecard.com/secure/ssl-gateway'
+  @homepage_url 'http://www.wirecard.com'
+
+end
---
+++ test/gateways/wire_card_test.exs
@@ -0,0 +1,16 @@
+defmodule Kuber.Hex.Gateways.WireCardTest do
+  use ExUnit.Case, async: false
+
+  import Mock
+
+  alias Kuber.Hex.{
+    CreditCard,
+    Address,
+    Response
+  }
+  alias Kuber.Hex.Gateways.Stripe, as: Gateway
+
+  setup do
+  end
+
+end
