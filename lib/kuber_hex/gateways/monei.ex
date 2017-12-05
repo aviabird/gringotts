baea3adc4934d2b21cb93be05e35e077861a6f3e
Ananya Bahadur <ananya95@gmail.com>
2017-12-05 16:13:30 +0530

    Added purchase, all calls return Response structs
    * Corrected the CVC and AVS translation maps
    * Use default argument for `currency` instead of passing it in `opts`
    * Fixed formatting of `amount`
    * Added a `commit` clause that returns :error if auth info is missing
    from `opts`
    * `purchase` != `authorize` + `capture`
      - This is because MONEI supports `purchase` separately (as "DB").

--- lib/kuber_hex/gateways/monei.ex
+++ lib/kuber_hex/gateways/monei.ex
@@ -5,13 +5,13 @@ defmodule Kuber.Hex.Gateways.Monei do
   commerce_billing and kuber_hex have some issues with their deps. `mix deps.get` throws a million warnings.
   Can't use Kuber.Hex.Gateways.Base.http as it is specifically tailored for stripe!
   There's no validation for opts, ie some required keys might be missing.
+    this will allow me to fail-fast, currently errors are caught in `commit` which is too late.
   Why don't we `use HTTPoison.Base`.
   Need a card brand field in CreditCard struct
   """
   
   use Kuber.Hex.Gateways.Base
   import Poison, only: [decode!: 1]
-  import HTTPoison
   alias Kuber.Hex.{CreditCard, Address, Response}
   
   @base_url "https://test.monei-api.net"
@@ -22,30 +22,46 @@ defmodule Kuber.Hex.Gateways.Monei do
   @cvc_code_translator %{
     "M" => "pass",
     "N" => "fail",
-    "P" => "unchecked"
+    "P" => "not_processed",
+    "U" => "issuer_unable",
+    "S" => "issuer_unable"
   }
 
   @avs_code_translator %{
-    "Y" => {"pass", "pass"},
+    "F" => {"pass", "pass"},
     "A" => {"pass", "fail"},
-    "B" => {"pass", "unchecked"},
     "Z" => {"fail", "pass"},
     "N" => {"fail", "fail"},
-    "P" => {"unchecked", "pass"},
-    "I" => {"unchecked", "unchecked"}
+    "U" => {"error", "error"},
+    "X" => {"absent", "absent"} # custom response code added by ananyab (this is never returned by MONEI
   }
   
   # MONEI supports payment by card, bank account and even something obscure: virtual account
   # opts has the auth keys.
-  def authorize(amount, card, opts) do # not just card, also bank!
+
+  @spec authorize(Float, String.t, CreditCard, Map) :: Response
+  def authorize(amount, currency \\ "EUR", card, opts) do # not just card, also bank!
     params = [paymentType: "PA",
-              amount: "92.00",
-              currency: Map.fetch!(opts, :default_currency)] ++ card_params(card)
+              amount: :erlang.float_to_binary(amount, decimals: 2),
+              currency: currency] ++ card_params(card)
     commit(:post, "payments", params, opts)
   end
 
-  def capture(amount, paymentId, opts) do
-    
+  @spec capture(Float, String.t, String.t, Map) :: Response
+  def capture(amount, currency \\ "EUR", <<paymentId::bytes-size(32)>>, opts) do
+    params = [paymentType: "CP",
+              amount: :erlang.float_to_binary(amount, decimals: 2),
+              currency: currency]
+    commit(:post, "payments/#{paymentId}", params, opts)
+  end
+
+  @spec purchase(Float, String.t, CreditCard, Map) :: Response
+  def purchase(amount, currency \\ "EUR", card, opts) do
+    params = [paymentType: "DB",
+              amount: :erlang.float_to_binary(amount, decimals: 2),
+              currency: currency] ++ card_params(card)
+    commit(:post, "payments", params, opts)
+  end
 
   defp card_params(card) do
     {expiration_year, expiration_month} = card.expiration
@@ -57,18 +73,56 @@ defmodule Kuber.Hex.Gateways.Monei do
      "paymentBrand": "VISA"] # DANGER!
   end
 
-  def commit(method, endpoint, params, opts = %{userId: userId,
-                                                password: password,
-                                                entityId: entityId}) do
+  def commit(method, endpoint, params, %{userId: userId,
+                                         password: password,
+                                         entityId: entityId}) do
     body = params ++ ["authentication.userId": userId,
                       "authentication.password": password,
                       "authentication.entityId": entityId]
     url = "#{@base_url}/#{@version}/#{endpoint}"
+    IO.inspect body
     method
     |> HTTPoison.request(url, {:form, body}, @default_headers)
-    # |> respond
+    |> respond
+  end
+  def commit(_method, _endpoint, _params, _opts) do
+    Response.error(reason: "Authorization fields missing")
   end
 
+  def respond({:ok, %{status_code: 200, body: body}}) do
+    data = decode!(body)
+    {code, description, _risk_score, cvc_result, avs_result} = verification_result(data)
+    Response.success(authorization: data["id"],
+      cvc_result: cvc_result,
+      avs_result: avs_result,
+      raw: {:json, data},
+      code: code,
+      reason: description)
+  end
+
+  @doc"""
+  MONEI will respond with an HTML message if status code is not 200.
+  """
+  def respond({:ok, %{status_code: status_code, body: body}}) do
+    Response.error(code: status_code, raw: {:html, body})
+  end
+
+  defp verification_result(%{"result" => result, "risk" => risk}) do
+    IO.inspect result
+    cvc_result = @cvc_code_translator[result["cvvResponse"]]
+    {address, zip_code} = case result["avsResponse"] do
+      nil -> @avs_code_translator["X"] # custom response code added by ananyab (this is never returned by MONEI
+      avs -> @avs_code_translator[avs]
+    end
+    {result["code"],
+     result["description"],
+     risk["score"],
+     cvc_result,
+     %{address: address, zip_code: zip_code}}
+  end
+
+  defp verification_result(_), do: {nil, nil, nil, nil, nil}
+      
 end
 
 """
@@ -79,6 +133,13 @@ cc = %CreditCard{
 name: "Jo Doe",
 number: "4200000000000000",
 expiration: {2019, 12},
+cvc:  "123"
+}
+
+bad_cc = %CreditCard{
+name: "Jo Doe",
+number: "4200000000000000",
+expiration: {2011, 12},
 cvc:  "123"
 }
 
