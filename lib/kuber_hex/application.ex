b899254fa71bd62cd55e2764648d6b3546573752
Ashish Singh <aitashish173@gmail.com>
2017-12-11 16:13:40 +0530

    Application start restructure
    * moved application related code to application.ex
    * changed the starting module for application in mix.ex

---
+++ lib/kuber_hex/application.ex
@@ -0,0 +1,33 @@
+defmodule Kuber.Hex.Application do
+  @moduledoc ~S"""
+  Has the supervision tree which monitors all the workers
+  that are handling the payments.
+  """
+
+  use Application
+
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      # Define workers and child supervisors to be supervised
+      # worker(Kuber.Hex.Worker, [arg1, arg2, arg3])
+      worker(
+        Kuber.Hex.Worker,
+        [
+          Application.get_env(:kuber_hex, Kuber.Hex)[:adapter], # gateway 
+          Application.get_env(:kuber_hex, Kuber.Hex) # options
+          # Experimental
+          # TODO: This is exposed from the config and is later used to call methods of the lib.
+          # [name: Application.get_env(:kuber_hex, Kuber.Hex)[:worker_process_name]]
+        ])
+    ]
+
+    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Kuber.Hex.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+
+end
