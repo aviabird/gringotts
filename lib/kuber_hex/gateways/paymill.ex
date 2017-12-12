09903ab46469a39ebd673651a07034d657d48b97
Sagar Karwande <sagar_karwande123@yahoo.com>
2017-12-12 17:51:50 +0530

    basic token generation using card

--- lib/kuber_hex/gateways/paymill.ex
+++ lib/kuber_hex/gateways/paymill.ex
@@ -1,5 +1,6 @@
 defmodule Kuber.Hex.Gateways.Paymill do
   use Kuber.Hex.Gateways.Base
+  alias Kuber.Hex.{ CreditCard, Address, Response}
 
   @supported_countries ~w(AD AT BE BG CH CY CZ DE DK EE ES FI FO FR GB
                           GI GR HR HU IE IL IM IS IT LI LT LU LV MC MT
@@ -12,8 +13,47 @@ defmodule Kuber.Hex.Gateways.Paymill do
   @default_currency "EUR"
   @live_url "https://api.paymill.com/v2/"
 
-  def authorize(amount, card, options) do
 
+  @private_key "8f16b021d4fb1f8d9263cbe346f32688"
+  @public_key "72294854039fcf7fd55eaeeb594577e7"
+
+  @credit_card %CreditCard{
+    name: "Sagar Karwande",
+    number: "4111111111111111",
+    expiration: {12, 2018},
+    cvc: 123
+  }
+
+  def authorize(amount, card, options) do
+    :post
+    |> HTTPoison.request()
+  end
+
+  defp set_username do
+    [{"Authorization", "Basic #{Base.encode64(@private_key)}"}]
+  end
+
+  def get_headers() do
+    [] ++ set_username
+  end
+
+  def get_save_card_params() do
+    [ {"transaction.mode" , "CONNECTOR_TEST"},
+      {"channel.id" , "72294854039fcf7fd55eaeeb594577e7"},
+      {"jsonPFunction" , "jsonPFunction"},
+      {"account.number" , "4111111111111111"},
+      {"account.expiry.month" , "12"},
+      {"account.expiry.year" , "2018"},
+      {"account.verification" , "123"},
+      {"account.holder" , "Sagar Karwande"},
+      {"presentation.amount3D" , "120"},
+      {"presentation.currency3D" , "EUR"}
+    ]
+  end
+
+  def save_card(card, options) do
+    :get
+    |> HTTPoison.request("https://test-token.paymill.com/","",get_headers(), [params: get_save_card_params()])
   end
 
   defp commit(method, action, parameters) do
