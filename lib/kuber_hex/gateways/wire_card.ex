661d489dddf4bcf144799cfb5e2d7d6c4157285a
voidzero <kartikjagdale11@gmail.com>
2017-12-13 20:23:03 +0530

    Add module doc for wirecard

--- lib/kuber_hex/gateways/wire_card.ex
+++ lib/kuber_hex/gateways/wire_card.ex
@@ -1,6 +1,35 @@
 # call => Kuber.Hex.Gateways.WireCard.authorize(100, creditcard, options)
 import XmlBuilder
 defmodule Kuber.Hex.Gateways.WireCard do
+  @moduledoc """
+    WireCard System Plugins - Terms of Use
+
+    The plugins offered are provided free of charge by Wirecard AG and are explicitly not part
+    of the Wirecard AG range of products and services.
+
+    They have been tested and approved for full functionality in the standard configuration
+    (status on delivery) of the corresponding shop system. They are under General Public
+    License Version 3 (GPLv3) and can be used, developed and passed on to third parties under
+    the same terms.
+
+    However, Wirecard AG does not provide any guarantee or accept any liability for any errors
+    occurring when used in an enhanced, customized shop system configuration.
+
+    Operation in an enhanced, customized configuration is at your own risk and requires a
+    comprehensive test phase by the user of the plugin.
+
+    Customers use the plugins at their own risk. Wirecard AG does not guarantee their full
+    functionality neither does Wirecard AG assume liability for any disadvantages related to
+    the use of the plugins. Additionally, Wirecard AG does not guarantee the full functionality
+    for customized shop systems or installed plugins of other vendors of plugins within the same
+    shop system.
+
+    Customers are responsible for testing the plugin's functionality before starting productive
+    operation.
+
+    By installing the plugin into the shop system the customer agrees to these terms of use.
+    Please do not use the plugin if you do not agree to these terms of use!
+  """
   @test_url "https://c3-test.wirecard.com/secure/ssl-gateway"
   @live_url "https://c3.wirecard.com/secure/ssl-gateway"
   @homepage_url "http://www.wirecard.com"
