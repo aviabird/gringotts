0052eea62b7db33b63f014bdd569bb502c8fd2c7
Chandra Shekhar <shekharindian099@gmail.com>
2017-12-13 18:08:16 +0530

    recieve opts params as keyword list

--- lib/kuber_hex/gateways/stripe.ex
+++ lib/kuber_hex/gateways/stripe.ex
@@ -10,38 +10,35 @@ defmodule Kuber.Hex.Gateways.Stripe do
     Response
   }
 
-  def authorize(amount, payment, opts \\ %{}) do
+  def authorize(amount, payment, opts \\ []) do
     params = create_params_for_auth_or_purchase(amount, payment, opts, false)
     commit(:post, "charges", params)
   end
 
-  def purchase(amount, payment, opts \\ %{}) do
+  def purchase(amount, payment, opts \\ []) do
     params = create_params_for_auth_or_purchase(amount, payment, opts)
     commit(:post, "charges", params)
   end
 
-  def capture(id, amount, opts \\ %{}) do
-    params = Enum.into(opts, [])
-      ++ amount_params(amount)
+  def capture(id, amount, opts \\ []) do
+    params = opts ++ amount_params(amount)
 
     commit(:post, "charges/#{id}/capture", params)
   end
 
-  def void(id, opts \\ %{}) do
-    params = Enum.into(opts, [])
+  def void(id, opts \\ []) do
+    params = opts
     commit(:post, "charges/#{id}/refund", params)
   end
 
-  def refund(id, amount, opts \\ %{}) do
-    params = Enum.into(opts, [])
-      ++ amount_params(amount)
+  def refund(amount, id, opts \\ []) do
+    params = opts ++ amount_params(amount)
 
     commit(:post, "charges/#{id}/refund", params)
   end
 
-  def store(card, opts \\ %{}) do
-    params = Enum.into(opts,[])  
-      ++ source_params(card)
+  def store(card, opts \\ []) do
+    params = opts ++ source_params(card)
     
     commit(:post, "customers", params)
   end
@@ -49,22 +46,22 @@ defmodule Kuber.Hex.Gateways.Stripe do
   def unstore(id), do: commit(:delete, "customers/#{id}")
 
   defp create_params_for_auth_or_purchase(amount, payment, opts, capture \\ true) do
-    Enum.into(opts, [])
-      ++ [capture: capture]
+    opts ++ [capture: capture]
       ++ amount_params(amount)
       ++ source_params(payment)
       # ++ customer_params(payment)
   end
 
+  defp create_card_token(params) do
+    commit(:post, "tokens", params)
+  end
+
   defp amount_params(amount), do: [amount: money_to_cents(amount)]
 
-  defp source_params(%{} = card) do
-    params = [
-      "card[number]": card.number,
-      "card[exp_year]": card.exp_year,
-      "card[exp_month]": card.exp_month,
-      "card[cvc]": card.cvc
-    ]
+  defp source_params(payment) do
+    params = 
+      card_params(payment) ++ 
+      address_params(payment)
 
     {:ok, %HTTPoison.Response{body: body}} = create_card_token(params)
     body
@@ -77,26 +74,33 @@ defmodule Kuber.Hex.Gateways.Stripe do
 
   # defp customer_params(customer_id), do: [customer: customer_id]
 
+  defp card_params(%{} = card) do
+    [ "card[number]": card[:number],
+      "card[exp_year]": card[:exp_year],
+      "card[exp_month]": card[:exp_month],
+      "card[cvc]": card[:cvc]
+    ]    
+  end
+
+  defp card_params(_), do: []
+
   defp address_params(%{} = address) do
-    ["source[address_line1]": address.street1,
-     "source[address_line2]": address.street2,
-     "source[address_city]":  address.city,
-     "source[address_state]": address.region,
-     "source[address_zip]":   address.postal_code,
-     "source[address_country]": address.country]
+    [ "card[address_line1]": address[:street1],
+      "card[address_line2]": address[:street2],
+      "card[address_city]":  address[:city],
+      "card[address_state]": address[:region],
+      "card[address_zip]":   address[:postal_code],
+      "card[address_country]": address[:country]
+    ]
   end
 
   defp address_params(_), do: []
 
-  defp create_card_token(params) do
-    commit(:post, "tokens", params)
-  end
-
-  defp commit(method, path, params) do
+  defp commit(method, path, params \\ []) do
     headers = [{"Content-Type", "application/x-www-form-urlencoded"}, {"Authorization", "Bearer sk_test_vIX41hayC0BKrPWQerLuOMld"}]
     data = params_to_string(params)
 
     HTTPoison.request(method, "#{@base_url}/#{path}", data, headers)
   end
-  
+
 end
