f63f12d3357735ff362158cef0763f1382f4b1cf
Chandra Shekhar <shekharindian099@gmail.com>
2017-12-14 19:57:41 +0530

    set config module attribute for testing

--- test/gateways/stripe_test.exs
+++ test/gateways/stripe_test.exs
@@ -20,13 +20,13 @@ defmodule Kuber.Hex.Gateways.StripeTest do
     postal_code: "11111"
   }
 
+  @config [config: [api_key: "sk_test_vIX41hayC0BKrPWQerLuOMld"]]
   @required_opts [currency: "usd"]
-
 
   describe "authorize/3" do
     test "should authorize wth required payment and required opts attrs" do
       amount = 5
-      response = Stripe.authorize(amount, @required_payment_attrs, @required_opts)
+      response = Stripe.authorize(amount, @required_payment_attrs, @config ++ @required_opts)
 
       assert Map.has_key?(response, "id")
       assert response["amount"] == 500
@@ -36,14 +36,14 @@ defmodule Kuber.Hex.Gateways.StripeTest do
 
     test "should not authorize if required payment attrs not present" do
       amount = 5
-      response = Stripe.authorize(amount, @optional_payment_attrs, @required_opts)
+      response = Stripe.authorize(amount, @optional_payment_attrs, @config ++ @required_opts)
 
       assert Map.has_key?(response, "error")
     end
 
     test "should not authorize if required opts not present" do
       amount = 5
-      response = Stripe.authorize(amount, @required_payment_attrs)
+      response = Stripe.authorize(amount, @required_payment_attrs, @config)
 
       assert Map.has_key?(response, "error")
     end
