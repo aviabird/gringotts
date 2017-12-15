5be9094d3ea4cffe9840b0c0021e9a9118296075
Sagar Karwande <sagar_karwande123@yahoo.com>
2017-12-15 18:04:58 +0530

    give actual config to methods

--- lib/kuber_hex/gateways/paymill.ex
+++ lib/kuber_hex/gateways/paymill.ex
@@ -2,6 +2,8 @@ defmodule Kuber.Hex.Gateways.Paymill do
   use Kuber.Hex.Gateways.Base
   alias Kuber.Hex.{ CreditCard, Address, Response}
 
+  use Kuber.Hex.Adapter, required_config: [:private_key, :public_key]
+
   @supported_countries ~w(AD AT BE BG CH CY CZ DE DK EE ES FI FO FR GB
                           GI GR HR HU IE IL IM IS IT LI LT LU LV MC MT
                           NL NO PL PT RO SE SI SK TR VA)
@@ -12,10 +14,7 @@ defmodule Kuber.Hex.Gateways.Paymill do
   @money_format :cents
   @default_currency "EUR"
   @live_url "https://api.paymill.com/v2.1/"
-
-
-  @private_key "8f16b021d4fb1f8d9263cbe346f32688"
-  @public_key "72294854039fcf7fd55eaeeb594577e7"
+  @headers [{"Content-Type", "application/x-www-form-urlencoded"}]
 
   @credit_card %CreditCard{
     name: "Sagar Karwande",
@@ -25,10 +24,12 @@ defmodule Kuber.Hex.Gateways.Paymill do
   }
 
   def save_card(card, options) do
-    {:ok, %HTTPoison.Response{body: response}} =
-    :get |> HTTPoison.request(get_save_card_url(),"",get_headers(), [params: get_save_card_params(card, options)])
+    {:ok, %HTTPoison.Response{body: response}} = HTTPoison.get(
+        get_save_card_url(),
+        get_headers(options),
+        params: get_save_card_params(card, options))
 
-    response |> parse_card_response
+     parse_card_response(response)
   end
 
   defp get_save_card_url(), do: "https://test-token.paymill.com/"
@@ -44,64 +45,62 @@ defmodule Kuber.Hex.Gateways.Paymill do
   def capture(amount, authorization, options) do
     post = add_amount([], amount, options) ++ [{"token", authorization}]
 
-    commit(:post, "transactions", post)
+    commit(:post, "transactions", post, options)
   end
 
   def void(authorization, options) do
-    commit(:delete, "preauthorizations/#{authorization}", options)
+    commit(:delete, "preauthorizations/#{authorization}", [], options)
   end
 
   def save(card, options) do
-    save_card(@credit_card, options)
+    save_card(card, options)
   end
 
   defp action_with_token(action, amount, card, options) do
     Keyword.put(options, :money, amount)
-    {:ok, response} = save_card(@credit_card, options)
-    card_token = response |> get_token
+    {:ok, response} = save_card(card, options)
+    card_token = get_token(response)
 
-    apply( __MODULE__, String.to_atom("#{action}_with_token"), [amount, card_token, options])
+    apply(__MODULE__, String.to_atom("#{action}_with_token"), [amount, card_token , options])
   end
 
   def authorize_with_token(money, card_token, options) do
-    post =
-    add_amount([], money, options)++[{"token", card_token}]
+    post = add_amount([], money, options) ++ [{"token", card_token}]
 
-    commit(:post, "preauthorizations", post)
+    commit(:post, "preauthorizations", post, options)
   end
 
   def purchase_with_token(money, card_token, options) do
-    post =
-    add_amount([], money, options)++[{"token", card_token}]
+    post = add_amount([], money, options) ++ [{"token", card_token}]
 
-    commit(:post, "transactions", post)
+    commit(:post, "transactions", post, options)
   end
 
   defp add_amount(post, money, options) do
     post ++ [{"amount", money}, {"currency", @default_currency}]
   end
 
-  defp set_username do
-    [{"Authorization", "Basic #{Base.encode64(@private_key)}"}]
+  defp set_username(options) do
+    [{"Authorization", "Basic #{Base.encode64(get_config(:private_key, options))}"}]
   end
 
-  def get_headers() do
-    [{"Content-Type", "application/x-www-form-urlencoded"}] ++ set_username
+  def get_headers(options) do
+    @headers ++ set_username(options)
   end
 
   def get_save_card_params(card, options) do
     {month, year} = card.expiration
 
     [ {"transaction.mode" , "CONNECTOR_TEST"},
-      {"channel.id" , "72294854039fcf7fd55eaeeb594577e7"},
+      {"channel.id" , get_config(:public_key, options)},
       {"jsonPFunction" , "jsonPFunction"},
       {"account.number" , card.number},
       {"account.expiry.month" , month},
       {"account.expiry.year" , year},
       {"account.verification" , card.cvc},
       {"account.holder" , card.name},
-      {"presentation.amount3D" , "120"},
-      {"presentation.currency3D" , "EUR"}
+      {"presentation.amount3D" , get_amount(options)},
+      {"presentation.currency3D" , get_currency(options)}
     ]
   end
 
@@ -112,15 +111,26 @@ defmodule Kuber.Hex.Gateways.Paymill do
     |> Poison.decode
   end
 
-  def get_token(response) do
-    response |> Kernel.get_in(["transaction", "identification", "uniqueId"])
+  defp get_currency(options) do
+    {:ok, currency} = Keyword.fetch(options, :currency)
+    currency
   end
 
-  defp commit(method, action, parameters \\ nil) do
-    body = [{"amount", "120"},{"currency" , "EUR"}, {"token", "tok_26bce989967d20e7061ff37c8410"}]
+  defp get_amount(options) do
+    {:ok, amount} = Keyword.fetch(options, :currency)
+    amount
+  end
 
-    method
-    |> HTTPoison.request(@live_url <> action, {:form, parameters }, get_headers, [])
+  def get_token(response) do
+    Kernel.get_in(response, ["transaction", "identification", "uniqueId"])
+  end
+
+  defp commit(method, action, parameters \\ nil, options) do
+    HTTPoison.request(method, @live_url <> action, {:form, parameters }, get_headers(options), [])
+  end
+
+  defp get_config(key, options) do
+    Kernel.get_in(options, [:config, key])
   end
 
 end
