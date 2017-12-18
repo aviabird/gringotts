d047cb697c1c61eac0ee2d75f150ca54748f9508
Ashish Singh <aitashish173@gmail.com>
2017-12-18 22:21:18 +0530

    updated more docs

--- lib/kuber_hex/credit_card.ex
+++ lib/kuber_hex/credit_card.ex
@@ -1,3 +1,16 @@
 defmodule Kuber.Hex.CreditCard do
+  @moduledoc ~S"""
+  CreditCard module defines the struct for the credit cards. 
+
+  It mostly has such as:-
+    * `number`: Credit card number
+    * `month`: Expiry month
+    * `year`: Expiration year
+    * `first_name`: First name of the card holder
+    * `last_name`: Last of the card holder
+    * `verification_code`: 3/4 digit code at the back of the card
+    * `brand`: VISA/MASTERCARD/MAESTRO/RUPAY etc.
+  """
+
   defstruct [:number, :month, :year, :first_name, :last_name, :verification_code, :brand]
 end
