cd441d67bd50bc80e2298917bc38e7a88a3d0d2a
Sagar Karwande <sagar_karwande123@yahoo.com>
2017-12-19 13:59:45 +0530

    add spec for public methods

--- lib/kuber_hex/gateways/paymill.ex
+++ lib/kuber_hex/gateways/paymill.ex
@@ -10,6 +10,7 @@ defmodule Kuber.Hex.Gateways.Paymill do
   @live_url "https://api.paymill.com/v2.1/"
   @headers [{"Content-Type", "application/x-www-form-urlencoded"}]
 
+  @spec save_card(CreditCard, Keyword) :: Response
   def save_card(card, options) do
     {:ok, %HTTPoison.Response{body: response}} = HTTPoison.get(
         get_save_card_url(),
@@ -19,28 +20,47 @@ defmodule Kuber.Hex.Gateways.Paymill do
      parse_card_response(response)
   end
 
+  @spec authorize(number, String.t | CreditCard, Keyword) :: Response
   def authorize(amount, card_or_token, options) do
     Keyword.put(options, :money, amount)
     action_with_token(:authorize, amount, card_or_token, options)
   end
 
+  @spec purchase(number, CreditCard, Keyword) :: Response
   def purchase(amount, card, options) do
     Keyword.put(options, :money, amount)
     action_with_token(:purchase, amount, card, options)
   end
 
+  @spec capture(number, String.t, Keyword) :: Response
   def capture(amount, authorization, options) do
     post = add_amount([], amount, options) ++ [{"token", authorization}]
 
     commit(:post, "transactions", post, options)
   end
 
+  @spec void(String.t, Keyword) :: Response
   def void(authorization, options) do
     commit(:delete, "preauthorizations/#{authorization}", [], options)
   end
 
+  @spec save(CreditCard, Keyword) :: Response
   def save(card, options) do
     save_card(card, options)
+  end
+
+  @spec authorize_with_token(number, String.t, Keyword) :: term
+  def authorize_with_token(money, card_token, options) do
+    post = add_amount([], money, options) ++ [{"token", card_token}]
+
+    commit(:post, "preauthorizations", post, options)
+  end
+
+  @spec purchase_with_token(number, String.t, Keyword) :: term
+  def purchase_with_token(money, card_token, options) do
+    post = add_amount([], money, options) ++ [{"token", card_token}]
+
+    commit(:post, "transactions", post, options)
   end
 
   defp action_with_token(action, amount, "tok_" <> id = card_token, options) do
@@ -54,19 +74,7 @@ defmodule Kuber.Hex.Gateways.Paymill do
     apply(__MODULE__, String.to_atom("#{action}_with_token"), [amount, card_token , options])
   end
 
-  def authorize_with_token(money, card_token, options) do
-    post = add_amount([], money, options) ++ [{"token", card_token}]
-
-    commit(:post, "preauthorizations", post, options)
-  end
-
-  def purchase_with_token(money, card_token, options) do
-    post = add_amount([], money, options) ++ [{"token", card_token}]
-
-    commit(:post, "transactions", post, options)
-  end
-
-  def get_save_card_params(card, options) do
+  defp get_save_card_params(card, options) do
     [ {"transaction.mode" , "CONNECTOR_TEST"},
       {"channel.id" , get_config(:public_key, options)},
       {"jsonPFunction" , "jsonPFunction"},
