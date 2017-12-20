1a1fba9900a0928a212e13ff91f5b7885c700bf3
Gopal Shimpi <gopalshimpi@gmail.com>
2017-12-20 14:58:16 +0530

    Final commit before PR, Removed verify method.

--- lib/gringotts/gateways/cams.ex
+++ lib/gringotts/gateways/cams.ex
@@ -16,8 +16,6 @@ defmodule Gringotts.Gateways.Cams do
     * **Refund**    In  `refund/3`
 
     * **Void**      In  `void/2`
-  
-    * **Verify**    In  `verify/2`
   """
   @live_url  "https://secure.centralams.com/gw/api/transact.php"
   @supported_countries  ["US"]
@@ -46,6 +44,18 @@ defmodule Gringotts.Gateways.Cams do
     Required fields in credit card are `Credit Card Number` & `Expiration Date`.
     Whereas `options` contains other information like billing address,order information etc. 
     After successful transaction it returns response containing **authcode** & **transactionid**.
+
+  ## Examples
+      payment = %{
+        number: "4111111111111111", month: 11, year: 2018,
+        first_name: "Longbob", last_name: "Longsen",
+        verification_code: "123", brand: "visa"
+      }
+
+      options = [currancy: "USD"]
+      money   = 100
+      
+      iex> Gringotts.purchase(:payment_worker, Gringotts.Gateways.Cams, money, payment, options)
   """
   @spec purchase(number, CreditCard, Keyword) :: Response
   def purchase(money, payment, options) do
@@ -57,17 +67,29 @@ defmodule Gringotts.Gateways.Cams do
   end
 
   @doc """
-    Use this method for authorising the credit card for particular transaction. 
+    Use this method for authorizing the credit card for particular transaction. 
 
 
-    `authorize/3` method only authorise the transaction,it does not transfer the funds.
-    After authorised a transaction, we need to call `capture/3` method to complete the transaction.
-    After successful authorisation it returns response containing **authcode** and **transactionid**.
+    `authorize/3` method only authorize the transaction,it does not transfer the funds.
+    After authorized a transaction, we need to call `capture/3` method to complete the transaction.
+    After successful authorization it returns response containing **authcode** and **transactionid**.
     We required **authcode**,**transactionid** and **money** for capturing transaction later on.
     It perform operation by taking `money`, `payment` (credit card details) & `options` as parameters.
     Here `money` is required field which contains amount to be deducted. 
     Required fields in credit card are `Credit Card Number` & `Expiration Date`.
     Whereas `options` contains other information like billing address,order information etc. 
+    
+  ## Examples
+      payment = %{
+        number: "4111111111111111", month: 11, year: 2018,
+        first_name: "Longbob", last_name: "Longsen",
+        verification_code: "123", brand: "visa"
+      }
+
+      options = [currancy: "USD"]
+      money   = 100
+      
+      iex> Gringotts.authorize(:payment_worker, Gringotts.Gateways.Cams, money, payment, options)
   """
   @spec authorize(number, CreditCard, Keyword) :: Response
   def authorize(money, payment, options) do
@@ -79,13 +101,22 @@ defmodule Gringotts.Gateways.Cams do
   end
  
   @doc """
-    Use this method for capture the amount of the authorised transaction which is previously authorised by `authorize/3` method.
+    Use this method for capture the amount of the authorized transaction which is previously authorized by `authorize/3` method.
 
 
-    It take `money`, `authorization` and `options` as parameters.
+    It takes `money`, `authorization` and `options` as parameters.
     Where `money` is a amount to be captured and `authorization` is a response returned by `authorize/3` method.
     From response it takes `authcode` and `transactionid` for further processing. 
     Both *money* and *authorization* are required fields, whereas `options` are as same as `authorize/3` and `purchase/3` methods.
+  
+  
+  ## Examples
+
+      authorization = "response=1&responsetext=SUCCESS&authcode=123456&transactionid=3904093075&avsresponse=N&cvvresponse=N&orderid=&type=sale&response_code=100"
+      options = [currancy: "USD"]
+      money   = 100
+      
+      iex> Gringotts.capture(:payment_worker, Gringotts.Gateways.Cams, money, authorization, options)
   """
   @spec capture(number, String.t, Keyword) :: Response
   def capture(money, authorization, options) do
@@ -102,10 +133,18 @@ defmodule Gringotts.Gateways.Cams do
     Successful transaction can be refunded after settlement or any time.
     It requires *transactionid* for refund the specified amount back to authorized payment source. 
     Only purchased(sale) transactions can be refund based on thier `transactionid`.
-    It take `money`, `authorization` and `options` as parameters.
+    It takes `money`, `authorization` and `options` as parameters.
     Where `money` is a amount to be refund and `authorization` is a response returned by `purchase/3` method.
     From response it takes `authcode` and `transactionid` for further processing. 
     Both *money* and *authorization* are required fields, whereas `options` are as same as `authorize/3`, `purchase/3` and `capture/3` methods.
+
+  ## Examples
+
+      authorization = "response=1&responsetext=SUCCESS&authcode=123456&transactionid=3904093075&avsresponse=N&cvvresponse=N&orderid=&type=sale&response_code=100"
+      options = [currancy: "USD"]
+      money   = 100
+      
+      iex> Gringotts.refund(:payment_worker, Gringotts.Gateways.Cams, money, authorization, options)
   """
   @spec refund(number, String.t, Keyword) :: Response
   def refund(money, authorization, options) do
@@ -122,24 +161,17 @@ defmodule Gringotts.Gateways.Cams do
     It is use to cancel the purchase(sale) transaction before settlement.
     Authorised transaction can be canceled, but once it captured, it can not be canceled.
     It requires *transactionid* to cancle transaction.Amount returned to the authorized payment source.
+  ## Examples
+
+      authorization = "response=1&responsetext=SUCCESS&authcode=123456&transactionid=3904093075&avsresponse=N&cvvresponse=N&orderid=&type=sale&response_code=100"
+      options = []
+      
+      iex> Gringotts.void(:payment_worker, Gringotts.Gateways.Cams, authorization, options)
   """
   @spec void(String.t, Keyword) :: Response
   def void(authorization , options) do  
     post = extract_auth([], authorization)
     commit("void", post, options)
-  end
-
-  @doc """
-  This mehthod is for only verification purpose for credit card.
-  Here we need to provide CreditCard info. 
-  """
-  @spec verify(CreditCard, Keyword) :: Response
-  def verify(credit_card, options) do
-    post = []
-          |> add_invoice( 0, options)
-          |> add_payment(credit_card)
-          |> add_address(credit_card, options)
-    commit("verify", post, options)
   end
 
   # private methods
@@ -211,42 +243,3 @@ defmodule Gringotts.Gateways.Cams do
     |> Keyword.put(:authcode, String.to_integer(response_body["authcode"]))  
   end
 end
-# payment = %CreditCard{
-#   number: "4111111111111111",
-#   month: 11,
-#   year: 2018,
-#   first_name: "Longbob",
-#   last_name: "Longsen",
-#   verification_code: "123",
-#   brand: "visa"
-# }
-# credit_card = %CreditCard{
-#   number: "4242424242424242",
-#   month: 11,
-#   year: 2018,
-#   first_name: "Longbob",
-#   last_name: "Longsen",
-#   verification_code: "123",
-#   brand: "visa"
-# }
-# address = %{
-#   name:     "Jim Smith",
-#   address1: "456 My Street",
-#   address2: "Apt 1",
-#   company:  "Widgets Inc",
-#   city:     "Ottawa",
-#   state:    "ON",
-#   zip:      "K1C2N6",
-#   country:  "CA",
-#   phone:    "(555)555-5555",
-#   fax:      "(555)555-6666"
-# }
-# options = [
-#   config: %{
-#             username: "testintegrationc",
-#             password: "password9"
-#           },
-#   order_id: 1,
-#   billing_address: address,
-#   description: "Store Purchase",
-# ] 
