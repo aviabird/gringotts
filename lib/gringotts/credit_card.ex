6897a21d3c7aa2018847b2199dfd8c8ea2129ca4
Ananya Bahadur <ananya95@gmail.com>
2017-12-24 19:26:29 +0530

    Defines the CreditCard "type" and adds docs
    \# Why `CreditCard.t`?
    
    If you read up on how to document Structs, you'll realise that
    `CreditCard`, (the module) should not be used as a "type" -- because it
    is a module, just a bunch of functions. This is related to the confusion
    between `String` and `String.t`.
    
    Composite types must be defined using @type. This helps static analysers
    like Dialyser to actually perform type-checking.
    
    \## Real World example
    
    In the excellent swoosh lib:
    https://github.com/swoosh/swoosh/search?utf8=%E2%9C%93&q=type+t+%3A%3A+%25__MODULE__%7Btype&type=
    
    References:
    https://www.reddit.com/r/elixir/comments/4v2ss2/help_me_understand_types_in_documentation_specs/
    http://elixir-recipes.github.io/types/type-checking/
    https://hexdocs.pm/elixir/typespecs.html#user-defined-types
    https://stackoverflow.com/questions/29977776/what-does-type-t-module-mean-in-elixir
    
    \# Other
    
    Note that `:year` and `:month` are **integers**!

--- lib/gringotts/credit_card.ex
+++ lib/gringotts/credit_card.ex
@@ -1,16 +1,47 @@
 defmodule Gringotts.CreditCard do
-  @moduledoc ~S"""
-  CreditCard module defines the struct for the credit cards. 
-
-  It mostly has such as:-
-    * `number`: Credit card number
-    * `month`: Expiry month
-    * `year`: Expiration year
-    * `first_name`: First name of the card holder
-    * `last_name`: Last of the card holder
-    * `verification_code`: 3/4 digit code at the back of the card
-    * `brand`: VISA/MASTERCARD/MAESTRO/RUPAY etc.
+  @moduledoc """
+  Defines a `Struct` for (credit) cards and some utilities.
   """
 
   defstruct [:number, :month, :year, :first_name, :last_name, :verification_code, :brand]
+  @typedoc """
+  Represents a Credit Card.
+
+  | Field               | Type       | Description                                  |
+  | -----               | ----       | -----------                                  |
+  | `number`            | `string`   | The card number.                             |
+  | `month`             | `integer`  | Month of expiry (a number in the `1..12`\
+                                       range).                                      |
+  | `year`              | `integer`  | Year of expiry.                              |
+  | `first_name`        | `string`   | First name of the card holder (as on card).  |
+  | `last_name`         | `string`   | Last name of the card holder (as on card).   |
+  | `verification_code` | `string`   | The [Card Verification Code][cvc], usually\
+                                       a 3-4 digit number on the back of the card.  |
+  | `brand`             | `string`   | The brand name of the card network (in\
+                                       some cases also the card issuer) in\
+                                       UPPERCASE. Some popular card networks\
+                                       are [Visa][visa], [MasterCard][mc],\
+                                       [Maestro][mo], [Diner's Club][dc] etc.       |
+
+  [cvc]: https://en.wikipedia.org/wiki/Card_security_code
+  [visa]: https://usa.visa.com
+  [mc]: https://www.mastercard.us/en-us.html
+  [mo]: http://www.maestrocard.com/gateway/index.html
+  [dc]: http://www.dinersclub.com/
+  """
+  @type t :: %__MODULE__{number: String.t,
+                         month: 1..12,
+                         year: non_neg_integer,
+                         first_name: String.t,
+                         last_name: String.t,
+                         verification_code: String.t,
+                         brand: String.t}
+
+  @doc """
+  Returns the full name of the card holder.
+
+  Joins `first_name` and `last_name` with a space in between.
+  """
+  @spec full_name(t) :: String.t
+  def full_name(card), do: "#{card.first_name} #{card.last_name}"
 end
