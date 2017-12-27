0e5972b93b1f404cb35a0b079633664ff5e7601c
Gopal Shimpi <gopalshimpi@gmail.com>
2017-12-27 20:52:14 +0530

    fixed indentation

--- test/mocks/cams_mock.exs
+++ test/mocks/cams_mock.exs
@@ -1,5 +1,6 @@
 defmodule Gringotts.Gateways.CamsMock do
 <<<<<<< HEAD
+<<<<<<< HEAD
   def successful_purchase do
     {:ok,
     %HTTPoison.Response{
@@ -8,6 +9,11 @@ defmodule Gringotts.Gateways.CamsMock do
 		{:ok,
 		%HTTPoison.Response{
 >>>>>>> 9c72b2a... Mock are indentate
+=======
+  def successful_purchase do
+    {:ok,
+    %HTTPoison.Response{
+>>>>>>> e35d058... fixed indentation
       body: "response=1&responsetext=SUCCESS&authcode=123456&transactionid=3916017714&avsresponse=N&cvvresponse=N&orderid=&type=sale&response_code=100",
       headers: [
         {"Date", "Thu, 21 Dec 2017 12:45:16 GMT"}, 
@@ -54,21 +60,21 @@ defmodule Gringotts.Gateways.CamsMock do
   end
   
   def failed_purchase_with_bad_credit_card do
-		{:ok,
-		%HTTPoison.Response{
+    {:ok,
+    %HTTPoison.Response{
       body: "response=3&responsetext=Invalid Credit Card Number REFID:3502947912&authcode=&transactionid=&avsresponse=&cvvresponse=&orderid=&type=sale&response_code=300",
-		  headers: [
+      headers: [
         {"Date", "Thu, 21 Dec 2017 13:20:08 GMT"},
         {"Server", "Apache"},
         {"Content-Length", "155"}, 
         {"Content-Type", "text/html; charset=UTF-8"}
       ],
-		request_url: "https://secure.centralams.com/gw/api/transact.php",
-		status_code: 200}}			 
-	end
-	def failed_purchase_with_bad_money do
-		{:ok,
-		%HTTPoison.Response{
+    request_url: "https://secure.centralams.com/gw/api/transact.php",
+    status_code: 200}}			 
+  end
+  def failed_purchase_with_bad_money do
+    {:ok,
+    %HTTPoison.Response{
       body: "response=3&responsetext=Invalid amount REFID:3502949755&authcode=&transactionid=&avsresponse=&cvvresponse=&orderid=&type=sale&response_code=300",
       headers: [
         {"Date", "Thu, 21 Dec 2017 13:50:20 GMT"}, 
@@ -76,11 +82,11 @@ defmodule Gringotts.Gateways.CamsMock do
         {"Content-Length", "143"},
         {"Content-Type", "text/html; charset=UTF-8"}
       ],
-		request_url: "https://secure.centralams.com/gw/api/transact.php",
-		status_code: 200}}
-	end
-	
-	def with_invalid_currency do
+    request_url: "https://secure.centralams.com/gw/api/transact.php",
+    status_code: 200}}
+  end
+  
+  def with_invalid_currency do
     {:ok,
     %HTTPoison.Response{
 >>>>>>> 9c72b2a... Mock are indentate
@@ -97,10 +103,14 @@ defmodule Gringotts.Gateways.CamsMock do
   
   def successful_capture do
 <<<<<<< HEAD
+<<<<<<< HEAD
     {:ok,
 =======
 		{:ok,
 >>>>>>> 9c72b2a... Mock are indentate
+=======
+    {:ok,
+>>>>>>> e35d058... fixed indentation
     %HTTPoison.Response{
       body: "response=1&responsetext=SUCCESS&authcode=123456&transactionid=3921111362&avsresponse=&cvvresponse=&orderid=&type=capture&response_code=100",
       headers: [
