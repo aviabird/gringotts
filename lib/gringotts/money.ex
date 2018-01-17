693d852cadc8411362e422ad91282d5b89baf915
Ananya Bahadur <ananya95@gmail.com>
2018-01-17 18:53:00 +0530

    Fixes #62 and #85
    * Also added an implementation of `to_string` for `ex_money`

--- lib/gringotts/money.ex
+++ lib/gringotts/money.ex
@@ -8,29 +8,85 @@ defprotocol Gringotts.Money do
   If your application is already using a supported Money library, just pass in
   the Money struct and things will work out of the box.
 
-  Otherwise, just wrap your `amount` with the `currency` together in a `Map` like so,
-      money = %{amount: Decimal.new(2017.18), currency: "USD"}
+  Otherwise, just wrap your `amount` with the `currency` together in a `Map`
+  like so,
 
-  and the API will accept it (as long as the currency is valid [ISO 4217 currency
-  code](https://www.iso.org/iso-4217-currency-codes.html)).
+      price = %{amount: Decimal.new(20.18), currency: "USD"}
+
+  and the API will accept it (as long as the currency is valid [ISO 4217
+  currency code](https://www.iso.org/iso-4217-currency-codes.html)).
+
+  ## Note on the `Any` implementation
+
+  Both `to_string` and `to_integer` assume that the precision for the `currency`
+  is 2 digits after decimal.
   """
   @fallback_to_any true
   @type t :: Gringotts.Money.t
   
-  @spec currency(t) :: String.t
   @doc """
   Returns the ISO 4217 compliant currency code associated with this sum of money.
 
   This must be an UPCASE `string`
   """
+  @spec currency(t) :: String.t
   def currency(money)
 
-  @spec value(t) :: Decimal.t
   @doc """
   Returns a Decimal representing the "worth" of this sum of money in the
   associated `currency`.
   """
+  @spec value(t) :: Decimal.t
   def value(money)
+
+  @doc """
+  Returns the ISO4217 `currency` code as string and `value` as an integer.
+  
+  Useful for gateways that require amount as integer (like cents instead of dollars)
+
+  ## Examples
+
+      # the money lib is aliased as "MoneyLib"
+  
+      iex> usd_price = MoneyLib.new(4.1234, :USD)
+      #MoneyLib<4.1234, "USD">
+      iex> Gringotts.Money.to_integer(usd_price)
+      {"USD", 412, -2}
+      
+      iex> bhd_price = MoneyLib.new(4.1234, :BHD)
+      #MoneyLib<4.1234, "USD">
+      iex> Gringotts.Money.to_integer(bhd_price)
+      {"BHD", 4123, -3}
+      # the Bahraini dinar is divided into 1000 fils unlike the dollar which is
+      # divided in 100 cents
+  """
+  @spec to_integer(Money.t) :: {currency :: String.t, value :: integer, exponent :: neg_integer}
+  def to_integer(money)
+
+  @doc """
+  Returns a tuple of ISO4217 `currency` code and the `value` as strings.
+
+  The `value` must match this regex: `~r[\\d+\\.\\d\\d{n}]` where `n+1` should
+  match the required precision for the `currency`.
+
+  > Gringotts will not (and cannot) validate this of course.
+
+  ## Examples
+
+      # the money lib is aliased as "MoneyLib"
+      
+      iex> usd_price = MoneyLib.new(4.1234, :USD)
+      #MoneyLib<4.1234, "USD">
+      iex> Gringotts.Money.to_string(usd_price)
+      {"USD", "4.12"}
+      
+      iex> bhd_price = MoneyLib.new(4.1234, :BHD)
+      #MoneyLib<4.1234, "BHD">
+      iex> Gringotts.Money.to_string(bhd_price)
+      {"BHD", "4.123"} 
+  """
+  @spec to_string(t) :: {currency :: String.t, value :: String.t}
+  def to_string(money)
 end
 
 # this implementation is used for dispatch on ex_money (and will also fire for
@@ -39,6 +95,22 @@ if Code.ensure_compiled?(Money) do
   defimpl Gringotts.Money, for: Money do
     def currency(money), do: money.currency |> Atom.to_string
     def value(money), do: money.amount
+
+    def to_integer(money) do
+      {_, int_value, exponent, _} = Money.to_integer_exp money
+      {currency(money), int_value, exponent}
+    end
+
+    def to_string(money) do
+      {:ok, currency_data} = Cldr.Currency.currency_for_code(currency(money))
+      reduced = Money.reduce money
+      {
+        currency(reduced),
+        value(reduced)
+        |> Decimal.round(currency_data.digits)
+        |> Decimal.to_string
+      }
+    end
   end  
 end
 
@@ -49,7 +121,26 @@ if Code.ensure_compiled?(Monetized.Money) do
   end
 end
 
+# Assumes that the currency is subdivided into 100 units
 defimpl Gringotts.Money, for: Any do
-  def currency(money), do: Map.get(money, :currency)
-  def value(money), do: Map.get(money, :amount) || Map.get(money, :value)
+  def currency(%{currency: currency}), do: currency
+  def value(%{value: %Decimal{} = value}), do: value
+
+  def to_integer(%{value: %Decimal{} = value, currency: currency}) do
+    {
+      currency,
+      value
+      |> Decimal.mult(Decimal.new(100))
+      |> Decimal.round(0)
+      |> Decimal.to_integer,
+      -2
+    }
+  end
+  
+  def to_string(%{value: %Decimal{} = value, currency: currency}) do
+    {
+      currency,
+      value |> Decimal.round(2) |> Decimal.to_string
+    }
+  end
 end
--- test/integration/money.exs
+++ test/integration/money.exs
@@ -6,14 +6,29 @@ defmodule Gringotts.Integration.Gateways.MoneyTest do
   @moduletag :integration
   
   @ex_money Money.new(42, :EUR)
+  @ex_money_long Money.new("42.126456", :EUR)
+  @ex_money_bhd Money.new(42, :BHD)
   
   describe "ex_money" do
-    test "amount is a Decimal.t" do
+    test "value is a Decimal.t" do
       assert match? %Decimal{}, MoneyProtocol.value(@ex_money) 
     end
 
-    test "currency is a String.t" do
-      assert match? currency when is_binary(currency), MoneyProtocol.currency(@ex_money)
+    test "currency is an upcase String.t" do
+      the_currency = MoneyProtocol.currency(@ex_money)
+      assert match? currency when is_binary(currency), the_currency
+      assert the_currency == String.upcase(the_currency)
+    end
+
+    test "to_integer" do
+      assert match? {"EUR", 4200, -2}, MoneyProtocol.to_integer(@ex_money)
+      assert match? {"BHD", 42000, -3}, MoneyProtocol.to_integer(@ex_money_bhd)
+    end
+
+    test "to_string" do
+      assert match? {"EUR", "42.00"}, MoneyProtocol.to_string(@ex_money)
+      assert match? {"EUR", "42.13"}, MoneyProtocol.to_string(@ex_money_long)
+      assert match? {"BHD", "42.000"}, MoneyProtocol.to_string(@ex_money_bhd)
     end
   end
 end
