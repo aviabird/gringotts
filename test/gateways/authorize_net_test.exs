a3fbcdc87d8527b09b9b1ba5e1a27bc1b427c2af
Ananya Bahadur <ananya95@gmail.com>
2018-02-09 17:48:04 +0530

    Refactored `commit` and corrected avs, cvc result
    Also added CAVV result filed to the response which is nil for
    MaasterCards (as per ANet docs).

--- test/gateways/authorize_net_test.exs
+++ test/gateways/authorize_net_test.exs
@@ -121,7 +121,7 @@ defmodule Gringotts.Gateways.AuthorizeNetTest do
   describe "purchase" do
     test "successful response with right params" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers ->
+        post: fn _url, _body, _headers ->
           MockResponse.successful_purchase_response()
         end do
         assert {:ok, response} = ANet.purchase(@amount, @card, @opts)
@@ -130,7 +130,7 @@ defmodule Gringotts.Gateways.AuthorizeNetTest do
 
     test "with bad card" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers ->
+        post: fn _url, _body, _headers ->
           MockResponse.bad_card_purchase_response()
         end do
         assert {:error, response} = ANet.purchase(@amount, @bad_card, @opts)
@@ -141,7 +141,7 @@ defmodule Gringotts.Gateways.AuthorizeNetTest do
   describe "authorize" do
     test "successful response with right params" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers ->
+        post: fn _url, _body, _headers ->
           MockResponse.successful_authorize_response()
         end do
         assert {:ok, response} = ANet.authorize(@amount, @card, @opts)
@@ -150,7 +150,7 @@ defmodule Gringotts.Gateways.AuthorizeNetTest do
 
     test "with bad card" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers ->
+        post: fn _url, _body, _headers ->
           MockResponse.bad_card_purchase_response()
         end do
         assert {:error, response} = ANet.authorize(@amount, @bad_card, @opts)
@@ -161,7 +161,7 @@ defmodule Gringotts.Gateways.AuthorizeNetTest do
   describe "capture" do
     test "successful response with right params" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers ->
+        post: fn _url, _body, _headers ->
           MockResponse.successful_capture_response()
         end do
         assert {:ok, response} = ANet.capture(@capture_id, @amount, @opts)
@@ -170,7 +170,7 @@ defmodule Gringotts.Gateways.AuthorizeNetTest do
 
     test "with bad transaction id" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers -> MockResponse.bad_id_capture() end do
+        post: fn _url, _body, _headers -> MockResponse.bad_id_capture() end do
         assert {:error, response} = ANet.capture(@capture_invalid_id, @amount, @opts)
       end
     end
@@ -179,7 +179,7 @@ defmodule Gringotts.Gateways.AuthorizeNetTest do
   describe "refund" do
     test "successful response with right params" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers ->
+        post: fn _url, _body, _headers ->
           MockResponse.successful_refund_response()
         end do
         assert {:ok, response} = ANet.refund(@amount, @refund_id, @opts_refund)
@@ -188,14 +188,14 @@ defmodule Gringotts.Gateways.AuthorizeNetTest do
 
     test "bad payment params" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers -> MockResponse.bad_card_refund() end do
+        post: fn _url, _body, _headers -> MockResponse.bad_card_refund() end do
         assert {:error, response} = ANet.refund(@amount, @refund_id, @opts_refund_bad_payment)
       end
     end
 
     test "debit less than refund amount" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers -> MockResponse.debit_less_than_refund() end do
+        post: fn _url, _body, _headers -> MockResponse.debit_less_than_refund() end do
         assert {:error, response} = ANet.refund(@amount, @refund_id, @opts_refund)
       end
     end
@@ -204,14 +204,14 @@ defmodule Gringotts.Gateways.AuthorizeNetTest do
   describe "void" do
     test "successful response with right params" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers -> MockResponse.successful_void() end do
+        post: fn _url, _body, _headers -> MockResponse.successful_void() end do
         assert {:ok, response} = ANet.void(@void_id, @opts)
       end
     end
 
     test "with bad transaction id" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers -> MockResponse.void_non_existent_id() end do
+        post: fn _url, _body, _headers -> MockResponse.void_non_existent_id() end do
         assert {:error, response} = ANet.void(@void_invalid_id, @opts)
       end
     end
@@ -220,21 +220,21 @@ defmodule Gringotts.Gateways.AuthorizeNetTest do
   describe "store" do
     test "successful response with right params" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers -> MockResponse.successful_store_response() end do
+        post: fn _url, _body, _headers -> MockResponse.successful_store_response() end do
         assert {:ok, response} = ANet.store(@card, @opts_store)
       end
     end
 
     test "successful response without validation and customer type" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers -> MockResponse.successful_store_response() end do
+        post: fn _url, _body, _headers -> MockResponse.successful_store_response() end do
         assert {:ok, response} = ANet.store(@card, @opts_store_without_validation)
       end
     end
 
     test "without any profile" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers ->
+        post: fn _url, _body, _headers ->
           MockResponse.store_without_profile_fields()
         end do
         assert {:error, response} = ANet.store(@card, @opts_store_no_profile)
@@ -245,7 +245,7 @@ defmodule Gringotts.Gateways.AuthorizeNetTest do
 
     test "with customer profile id" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers ->
+        post: fn _url, _body, _headers ->
           MockResponse.customer_payment_profile_success_response()
         end do
         assert {:ok, response} = ANet.store(@card, @opts_customer_profile)
@@ -256,7 +256,7 @@ defmodule Gringotts.Gateways.AuthorizeNetTest do
 
     test "successful response without valiadtion mode and customer type" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers -> MockResponse.successful_store_response() end do
+        post: fn _url, _body, _headers -> MockResponse.successful_store_response() end do
         assert {:ok, response} = ANet.store(@card, @opts_customer_profile_args)
       end
     end
@@ -265,7 +265,7 @@ defmodule Gringotts.Gateways.AuthorizeNetTest do
   describe "unstore" do
     test "successful response with right params" do
       with_mock HTTPoison,
-        request: fn _method, _url, _body, _headers ->
+        post: fn _url, _body, _headers ->
           MockResponse.successful_unstore_response()
         end do
         assert {:ok, response} = ANet.unstore(@unstore_id, @opts)
@@ -275,7 +275,7 @@ defmodule Gringotts.Gateways.AuthorizeNetTest do
 
   test "network error type non existent domain" do
     with_mock HTTPoison,
-      request: fn _method, _url, _body, _headers ->
+      post: fn _url, _body, _headers ->
         MockResponse.netwok_error_non_existent_domain()
       end do
       assert {:error, response} = ANet.purchase(@amount, @card, @opts)
