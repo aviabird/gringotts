24e357bec83702a6cba39ade506171d030536192
ravirocx <itsyourraviraj@gmail.com>
2018-03-29 21:00:49 +0530

    formatted all files

--- test/mocks/pin_pay_mock.exs
+++ test/mocks/pin_pay_mock.exs
@@ -1,9 +1,7 @@
 defmodule Gringotts.Gateways.PinpaymentsMock do
-
   # The module should include mock responses for test cases in pin_pay_test.exs.
   # e.g. 
   # def successful_purchase do 
   #  {:ok, %HTTPoison.Response{body: ~s[{data: "successful_purchase"}]}
   # end
-
 end
