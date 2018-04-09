40592473d20a66fa3f8eca76496a74034a83dd54
Ananya Bahadur <ananya95+git@gmail.com>
2018-04-09 12:19:12 +0530

    Add clause to catch/handle bad config format
    Closes #150
    Closes #136

--- lib/gringotts/adapter.ex
+++ lib/gringotts/adapter.ex
@@ -49,13 +49,19 @@ defmodule Gringotts.Adapter do
       Raises a run-time `ArgumentError` if any of the `required_config` values
       is not available or missing from the Application config.
       """
-      def validate_config(config) do
+      def validate_config(config) when is_list(config) do
         missing_keys =
           Enum.reduce(@required_config, [], fn key, missing_keys ->
             if config[key] in [nil, ""], do: [key | missing_keys], else: missing_keys
           end)
 
         raise_on_missing_config(missing_keys, config)
+      end
+
+      def validate_config(config) when is_map(config) do
+        config
+        |> Enum.into([])
+        |> validate_config
       end
 
       defp raise_on_missing_config([], _config), do: :ok
