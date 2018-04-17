d0bda62c3acc59ee3469382211c2a3bd2c2223be
siriusdark <subhampal198@gmail.com>
2018-04-17 23:23:45 +0530

    Applied elixer formatter.

--- test/integration/gateways/mercadopago_test.exs
+++ test/integration/gateways/mercadopago_test.exs
@@ -5,61 +5,70 @@ defmodule Gringotts.Integration.Gateways.MercadopagoTest do
   use ExVCR.Mock, adapter: ExVCR.Adapter.Hackney
   alias Gringotts.Gateways.Mercadopago, as: Gateway
 
-  @moduletag :integration
+  @moduletag integration: true
 
   @amount Money.new(45, :BRL)
   @sub_amount Money.new(30, :BRL)
   @good_card %Gringotts.CreditCard{
-            first_name: "Hermoine",
-            last_name: "Granger",
-            number: "4509953566233704",
-            year: 2030,
-            month: 07,
-            verification_code: "123",
-            brand: "VISA"
-        }
+    first_name: "Hermoine",
+    last_name: "Granger",
+    number: "4509953566233704",
+    year: 2030,
+    month: 07,
+    verification_code: "123",
+    brand: "VISA"
+  }
 
   @bad_card %Gringotts.CreditCard{
-            first_name: "Hermoine",
-            last_name: "Granger",
-            number: "4509953566233704",
-            year: 2000,
-            month: 07,
-            verification_code: "123",
-            brand: "VISA"
-        }
+    first_name: "Hermoine",
+    last_name: "Granger",
+    number: "4509953566233704",
+    year: 2000,
+    month: 07,
+    verification_code: "123",
+    brand: "VISA"
+  }
 
-  @good_opts [email: "hermoine@granger.com",
-         order_id: 123126,
-         customer_id: "311211654-YrXF6J0QikpIWX",
-         config: [access_token: "TEST-2774702803649645-031303-1b9d3d63acb57cdad3458d386eee62bd-307592510",
-                 public_key: "TEST-911f45a1-0560-4c16-915e-a8833830b29a"],
-         installments: 1
-        ]
-  @new_cutomer_good_opts [order_id: 123126,
-         config: [access_token: "TEST-2774702803649645-031303-1b9d3d63acb57cdad3458d386eee62bd-307592510",
-                 public_key: "TEST-911f45a1-0560-4c16-915e-a8833830b29a"],
-         installments: 1
-        ]
-  @new_cutomer_bad_opts [order_id: 123126,
-         config: [public_key: "TEST-911f45a1-0560-4c16-915e-a8833830b29a"],
-         installments: 1
-        ]
-  @bad_opts [email: "hermoine@granger.com",
-         order_id: 123126,
-         customer_id: "311211654-YrXF6J0QikpIWX",
-         config: [public_key: "TEST-911f45a1-0560-4c16-915e-a8833830b29a"],
-         installments: 1
-       ]
+  @good_opts [
+    email: "hermoine@granger.com",
+    order_id: 123_126,
+    customer_id: "311211654-YrXF6J0QikpIWX",
+    config: [
+      access_token: "TEST-2774702803649645-031303-1b9d3d63acb57cdad3458d386eee62bd-307592510",
+      public_key: "TEST-911f45a1-0560-4c16-915e-a8833830b29a"
+    ],
+    installments: 1
+  ]
+  @new_cutomer_good_opts [
+    order_id: 123_126,
+    config: [
+      access_token: "TEST-2774702803649645-031303-1b9d3d63acb57cdad3458d386eee62bd-307592510",
+      public_key: "TEST-911f45a1-0560-4c16-915e-a8833830b29a"
+    ],
+    installments: 1
+  ]
+  @new_cutomer_bad_opts [
+    order_id: 123_126,
+    config: [public_key: "TEST-911f45a1-0560-4c16-915e-a8833830b29a"],
+    installments: 1
+  ]
+  @bad_opts [
+    email: "hermoine@granger.com",
+    order_id: 123_126,
+    customer_id: "311211654-YrXF6J0QikpIWX",
+    config: [public_key: "TEST-911f45a1-0560-4c16-915e-a8833830b29a"],
+    installments: 1
+  ]
 
   def new_email_opts(good) do
     no1 = :rand.uniform(1_000_00) |> to_string
     no2 = :rand.uniform(1_000_00) |> to_string
     no3 = :rand.uniform(1_000_00) |> to_string
     email = "hp" <> no1 <> no2 <> no3 <> "@potter.com"
+
     case good do
-       true -> @new_cutomer_good_opts ++ [email: email]
-       _ -> @new_cutomer_bad_opts ++ [email: email]
+      true -> @new_cutomer_good_opts ++ [email: email]
+      _ -> @new_cutomer_bad_opts ++ [email: email]
     end
   end
 
@@ -132,6 +141,4 @@ defmodule Gringotts.Integration.Gateways.MercadopagoTest do
       end
     end
   end
-
 end
-
