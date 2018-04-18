1123360cfc5202773a9769ef5980c178ac0dcd81
siriusdark <subhampal198@gmail.com>
2018-04-18 23:58:36 +0530

    Added config as module attribute.

--- test/integration/gateways/mercadopago_test.exs
+++ test/integration/gateways/mercadopago_test.exs
@@ -9,6 +9,10 @@ defmodule Gringotts.Integration.Gateways.MercadopagoTest do
 
   @amount Money.new(45, :BRL)
   @sub_amount Money.new(30, :BRL)
+  @config [
+    access_token: "TEST-2774702803649645-031303-1b9d3d63acb57cdad3458d386eee62bd-307592510",
+    public_key: "TEST-911f45a1-0560-4c16-915e-a8833830b29a"
+  ]
   @good_card %Gringotts.CreditCard{
     first_name: "Hermoine",
     last_name: "Granger",
@@ -23,10 +27,7 @@ defmodule Gringotts.Integration.Gateways.MercadopagoTest do
     email: "hermoine@granger.com",
     order_id: 123_126,
     customer_id: "311211654-YrXF6J0QikpIWX",
-    config: [
-      access_token: "TEST-2774702803649645-031303-1b9d3d63acb57cdad3458d386eee62bd-307592510",
-      public_key: "TEST-911f45a1-0560-4c16-915e-a8833830b29a"
-    ],
+    config: @config,
     installments: 1
   ]
 
