b1b78917112c5a58859844564c9d692e6cbe6dd0
Ashish Singh <aitashish173@gmail.com>
2018-04-22 17:58:24 +0530

    Changes for release 1.1.0
    * Bumped version to 1.1.0
    * Not running integration tests on travis
    * Added Changelog for release 1.1.0(refer CHANGELOG.md)

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Gringotts.Mixfile do
   def project do
     [
       app: :gringotts,
-      version: "1.0.2",
+      version: "1.1.0",
       description: description(),
       package: [
         contributors: ["Aviabird Technologies"],
